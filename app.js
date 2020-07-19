2ca09c0a9b6aece898e63297238cb953794b75e2
colmclovin <thecanadianbaconstrip@gmail.com>
2020-07-19 17:15:41 -0600

    added tenorjs api

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 // Require Libraries
 const express = require('express');
 
+const Tenor = require("tenorjs").client({
+    "Key": "E8Q6U36USY92", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+    "MediaFilter": "minimal", // either minimal or basic, not case sensitive
+    "DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
+});
 // App Setup
 const app = express();
 
-// Middleware
+// Middleware kk
 const exphbs = require('express-handlebars');
-
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Routes
-/*
-app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl });
-  }); */
-  app.get('/', (req, res) => {
-      console.log(req.query);
-      res.render('home')});
 
-  app.get('/greetings/:name', (req, res) => 
-  {
-    const name = req.params.name;
-    res.render('greetings', {name});  
-  } );
+  
+
+  // Routes
+  app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "5")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 // Start Server
 
 app.listen(3000, () => {
